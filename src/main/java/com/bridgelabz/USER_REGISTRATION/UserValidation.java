fa105903ef545ddf1e238e03b42be2082b0e9c20
drs317lakshami <devrajkumar317@gmail.com>
2023-02-15 23:55:31 +0530

    Added :User Need to enter a valid last name

--- src/main/java/com/bridgelabz/USER_REGISTRATION/UserValidation.java
+++ src/main/java/com/bridgelabz/USER_REGISTRATION/UserValidation.java
@@ -3,9 +3,11 @@ package com.bridgelabz.USER_REGISTRATION;
 import java.util.regex.Pattern;
 
 public class UserValidation {
-
-	 public static final String VALIDATE_NAME="^[A-z]+[a-z]{2,}$";
-	    public boolean checkFirstName(String firstName) {
-	        return (Pattern.matches(VALIDATE_NAME, firstName));
-	    }
-	}
+    public static final String VALIDATE_NAME="^[A-z]+[a-z]{2,}$";
+    public boolean checkFirstName(String firstName) {
+        return (Pattern.matches(VALIDATE_NAME, firstName));
+    }
+    public Boolean checkLastName(String lastName) {
+        return (Pattern.matches(VALIDATE_NAME,lastName));
+    }
+}
--- src/test/java/com/bridgelabz/USER_REGISTRATION/RegistrationTest.java
+++ src/test/java/com/bridgelabz/USER_REGISTRATION/RegistrationTest.java
@@ -1,23 +1,28 @@
-package com.bridgelabz.USER_REGISTRATION;
+package com.brideglabz.junit;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 public class RegistrationTest {
-
-	 @Before
-	    static void Msg(){
-	        System.out.println("Welcome to User Registration Problem");
-	    }
-	    @After
-	    public void afterMsg(){
-	        System.out.println("Passed");
-	    }
-	    @Test
-	    void given_first_name_should_returnTrue() {
-	        UserValidation validation = new UserValidation();
-	        Boolean result = validation.checkFirstName("Aditya");
-	         Assertions.assertEquals(result, true);
-	    }
-	}
+    @BeforeAll
+    static void Msg(){
+        System.out.println("Welcome to User Registration Problem");
+    }
+    @AfterEach
+    public void afterMsg(){
+        System.out.println("Passed");
+    }
+    @Test
+    void given_first_name_should_returnTrue() {
+        UserValidation validation = new UserValidation();
+        Boolean result = validation.checkFirstName("Usha");
+        Assertions.assertEquals(result, true);
+    }
+    @Test
+    void given_last_name_should_returnTrue() {
+        UserValidation validation = new UserValidation();
+        Boolean result1 = validation.checkLastName("Ahirwar");
+        Assertions.assertEquals(result1, true);
+    }
+}
